80245989fd18cc2d3319b965cb9a972b9b889861
Mafuyu-CLBM <mondelo.clent1@gmail.com>
2026-02-10 13:39:22 +0800

    Add clent2.py with personal information and types

---
+++ clent2.py
@@ -0,0 +1,16 @@
+fullname = "Clent Lorence B. Mondelo"
+age = 19
+height = 1.7
+is_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+print()
+print("Age:", age)
+print(type(age))
+print()
+print("Height in meters:", height)
+print(type(height))
+print()
+print("Currently working?", is_working)
+print(type(is_working))
